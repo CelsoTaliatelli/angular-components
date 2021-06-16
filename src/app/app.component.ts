6fe30ffbd83c4510a88fe8e2fe808d43863faf24
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-06-16 09:59:15 -0300

    teste com pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,13 +17,14 @@ export class AppComponent implements OnInit{
       {property: 'nome',label: 'Nome', width: '200px'},
       {property:'cargo',label: 'Cargo', width: 150 },
       {property:'email',label: 'E-mail', width: 100},
-      {property:'status',label: 'Status', width: 80}
+      {property:'status',label: 'Status', width: 80},
+      {property:'data',label: 'Data', width: 80},
     ];
 
     this.data = [
-      {codigo: '1',nome:'Celso',cargo:'Engenheiro de Software',email:'celso@mail.com',status:'OK'},
-      {codigo: '2',nome:'Celso',cargo:'Engenheiro de Software',email:'celso@mail.com',status:'OK'},
-      {codigo: '3',nome:'Celso',cargo:'Engenheiro de Software',email:'celso@mail.com',status:'OK'}
+      {codigo: '1',nome:'Celso',cargo:'Engenheiro de Software',email:'celso@mail.com',data: new Date(),status:'OK'},
+      {codigo: '2',nome:'Celso',cargo:'Engenheiro de Software',email:'celso@mail.com',data: new Date(),status:'OK'},
+      {codigo: '3',nome:'Celso',cargo:'Engenheiro de Software',email:'celso@mail.com',data: new Date(),status:'OK'}
     ]
 
     console.log(this.columns);
